3741ca012684619ce7e4c645f01b2fe3b96e213b
Joseph Gros <wiley.joseph.gros@gmail.com>
2024-06-01 23:24:54 +0200

    yes

--- src/db/db.tsx
+++ src/db/db.tsx
@@ -2,8 +2,7 @@ import { connect } from "mongoose";
 
 const connectDB = async () => {
   try {
-    const mongoURI: string =
-      "mongodb+srv://wileyjosephgros:adminAPI@mtgvaultapi.ipkvptw.mongodb.net/MTGTombAPI?retryWrites=true&w=majority&appName=MTGVaultAPI";
+    const mongoURI: string = process.env.MongoURI || "";
     await connect(mongoURI);
     console.log("MongoDB Connected");
   } catch (err: any) {
